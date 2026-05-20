d3d841d925ab5951716acdac16b9c995cc63865f
joazis-quebra-codegos <joao.ds.zantti@gmail.com>
2026-05-19 22:30:13 -0300

    Update InscricaoController.java

--- src/main/java/org/example/eventos/controller/InscricaoController.java
+++ src/main/java/org/example/eventos/controller/InscricaoController.java
@@ -19,21 +19,25 @@ public class InscricaoController {
     @Autowired
     private InscricaoService service;
 
+    @Operation(summary = "Busca uma inscrição por Id")
     @GetMapping
     public InscricaoResponseDTO buscarPorId(@PathVariable Long id){
         return service.buscarPorId(id);
     }
 
+    @Operation(summary = "Lista todas as incrições")
     @GetMapping("/{id}")
     public List<InscricaoResponseDTO> listar(){
         return service.listarTodas();
     }
 
+    @Operation(summary = "Cadastra uma inscrição nova")
     @PostMapping
     public InscricaoResponseDTO cadastrar(@RequestBody @Valid InscricaoRequestDTO dto){
         return service.cadastrar(dto);
     }
 
+    @Operation(summary = "Deleta uma inscrição")
     @DeleteMapping("/{id}")
     public void deletar(@PathVariable Long id){
         service.deletar(id);
