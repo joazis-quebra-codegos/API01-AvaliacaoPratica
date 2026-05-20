f534b1cfae58b3c0c917e87adbb5734ec0050648
joazis-quebra-codegos <joao.ds.zantti@gmail.com>
2026-05-19 22:30:24 -0300

    Update LocalEventoController.java

--- src/main/java/org/example/eventos/controller/LocalEventoController.java
+++ src/main/java/org/example/eventos/controller/LocalEventoController.java
@@ -1,5 +1,6 @@
 package org.example.eventos.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.apache.logging.log4j.message.ReusableMessage;
@@ -19,26 +20,31 @@ public class LocalEventoController {
     @Autowired
     private LocalEventoService localEventoService;
 
+    @Operation(summary = "Busca um local por Id")
     @GetMapping("/{id}")
     public LocalEventoResponseDTO buscarPorId(@PathVariable Long id){
         return localEventoService.buscarPorId(id);
     }
 
+    @Operation(summary = "Lista todos os locais")
     @GetMapping
     public List<LocalEventoResponseDTO> listar(){
         return localEventoService.listar();
     }
 
+    @Operation(summary = "Cadastra um local novo")
     @PostMapping
     public LocalEventoResponseDTO cadastrar(@RequestBody @Valid LocalEventoRequestDTO dto){
         return localEventoService.cadastrar(dto);
     }
 
+    @Operation(summary = "Deleta um local")
     @DeleteMapping("/{id}")
     public void deletar(@PathVariable Long id){
         localEventoService.deletar(id);
     }
 
+    @Operation(summary = "Atualiza as informações de um local")
     @PutMapping("/{id}")
     public LocalEventoResponseDTO atualizar(@PathVariable Long id, @RequestBody @Valid LocalEventoRequestDTO dto){
         return localEventoService.atualizar(id, dto);
